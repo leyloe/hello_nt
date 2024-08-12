6d4ffe8fcd3b5b3df205bbc1aa52302aa78190df
fu3x11 <159873685+fu3x11@users.noreply.github.com>
2024-08-12 00:44:30 +0000

    Update build.zig

--- build.zig
+++ build.zig
@@ -10,17 +10,5 @@ pub fn build(b: *std.Build) void {
         .optimize = std.builtin.OptimizeMode.ReleaseSmall,
     });
 
-    build_exe_step(b, exe);
-    run_exe_step(b, exe);
-}
-
-fn build_exe_step(b: *std.Build, exe: *std.Build.Step.Compile) void {
     b.installArtifact(exe);
-}
-
-fn run_exe_step(b: *std.Build, exe: *std.Build.Step.Compile) void {
-    const run_exe = b.addRunArtifact(exe);
-
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
 }
